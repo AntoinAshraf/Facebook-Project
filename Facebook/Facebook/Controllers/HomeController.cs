38804b95425d0293577ae01dfeb636ec489f7a88
ahmed hamed <ahmedhamed121212@yahoo.com>
2020-03-14 12:57:27 +0200

    create project and ef setup

---
+++ Facebook/Facebook/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using FacebookDbContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Facebook.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FacebookDataContext facebookDataContext;
+        public HomeController(ILogger<HomeController> logger, FacebookDataContext _facebookDataContext)
+        {
+            _logger = logger;
+            facebookDataContext = _facebookDataContext;
+        }
+
+        public IActionResult Index()
+        {
+            var x = facebookDataContext.Users.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Facebook/Facebook/Models/DomainModels/Role.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FaceBook.Models
+{
+    public partial class Role
+    {
+        public Role()
+        {
+            RoleActions = new HashSet<RoleAction>();
+            Users = new HashSet<User>();
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual ICollection<RoleAction> RoleActions { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
