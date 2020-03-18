8ead7bafd67e65cd1d0dd2f8b8b8e6a07c335324
Pop <muhammedadel96@gmail.com>
2020-03-18 22:55:37 +0200

    Home Page V2

--- Facebook/Facebook/Startup.cs
+++ Facebook/Facebook/Startup.cs
@@ -80,8 +80,8 @@ namespace Facebook
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                //pattern: "{controller=Account}/{action=Register}/{id?}");
+                //pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Register}/{id?}");
             });
         }
     }
